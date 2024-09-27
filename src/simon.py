e7e50c6084f2b26586e88fc1a6161cd9bde5bd15
Simon Gao <simon.shubin@gmail.com>
2024-09-27 15:39:46 +0200

    add method

--- src/simon.py
+++ src/simon.py
@@ -73,3 +73,35 @@ class Simon:
         if self.is_point_in_convex_polygon(data):
             self.pointsInsidePolygon += 1
 
+    def readSecondPolygon(self, data):
+        self.polygon2 = data
+
+    def pointInsidePolygon(self, polygon, pointData):
+        point = [pointData[1], pointData[0]]
+        """
+        Determine if a point is inside a convex polygon using the ray-casting algorithm.
+
+        :param point: A tuple (x, y) representing the point.
+        :param polygon: A list of tuples [(x1, y1), (x2, y2), ...] representing the vertices of the polygon.
+        :return: True if the point is inside the polygon, False otherwise.
+        """
+        x, y = point[1], point[0]
+        n = len(polygon)
+        inside = False
+
+        p1x, p1y = polygon[0]
+        for i in range(n + 1):
+            p2x, p2y = polygon[i % n][0], polygon[i % n][1]
+            if y > min(p1y, p2y):
+                if y <= max(p1y, p2y):
+                    if x <= max(p1x, p2x):
+                        if p1y != p2y:
+                            xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
+                        if p1x == p2x or x <= xinters:
+                            inside = not inside
+            p1x, p1y = p2x, p2y
+
+        return inside
+
+    def addObservation(self, data):
+        pass
