3ec3cc63e05e99b6fcdb864ea02614d1f2c2a683
Simon Gao <simon.shubin@gmail.com>
2024-09-27 14:31:28 +0200

    change method

--- src/simon.py
+++ src/simon.py
@@ -1,15 +1,17 @@
 class Simon:
     def __init__(self) -> None:
         self.pointsInsideRect = 0
+        self.points = []
 
     # read a rectangle via north, east, south and west coords
     def readRect(self, n, o, s, w):
-        pass
-        # self.n = n
-        # self.o = o
-        # self.s = s
-        # self.w = w
+        self.n = n
+        self.o = o
+        self.s = s
+        self.w = w
 
     # add a point (x and y coords) 
-    def addPoint(self, l, m):
+    def addPoint(self, x, y):
         pass
+        # self.points.append((l, m))
+        # if self.w < l and l < self.o and m 
