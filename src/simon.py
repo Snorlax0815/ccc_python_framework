ed7daaf229a39dd16c77dff2f806fd723c660865
Simon Gao <simon.shubin@gmail.com>
2024-09-27 14:28:57 +0200

    temp

--- src/simon.py
+++ src/simon.py
@@ -1,2 +1,15 @@
 class Simon:
-    pass
+    def __init__(self) -> None:
+        self.pointsInsideRect = 0
+
+    # read a rectangle via north, east, south and west coords
+    def readRect(self, n, o, s, w):
+        pass
+        # self.n = n
+        # self.o = o
+        # self.s = s
+        # self.w = w
+
+    # add a point (x and y coords) 
+    def addPoint(self, l, m):
+        pass
